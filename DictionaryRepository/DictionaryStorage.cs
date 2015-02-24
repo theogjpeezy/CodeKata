695267037f1e9024cc557320412a237f5e3ef3a2
justin.cronin01@gmail.com <justin.cronin01@gmail.com>
2015-02-23 23:59:29 -0500

    Helps when I add all of the files

---
+++ DictionaryRepository/DictionaryStorage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace DictionaryRepository
+{
+    public class DictionaryStorage<T>
+    {
+        private readonly Dictionary<string, T> _dictionary;
+
+		public DictionaryStorage()
+		{
+			_dictionary = new Dictionary<string, T>();
+		}
+
+	    public IEnumerable<string> GetKeys() {
+	       return _dictionary.Keys;
+	    }
+
+		public T Get(string key)
+		{
+			return _dictionary[key];
+		}
+
+		public void Set(string key, T value)
+		{
+			_dictionary[key] = value;
+		}
+
+        public bool Exists(string key) {
+            return _dictionary.ContainsKey(key);
+        }
+    }
+}
---
+++ SOLID/HashTableStorage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HashTableRepository {
+
+	public class HashTableStorage<T> {
+	    private readonly Hashtable _hashtable;
+
+		public HashTableStorage()
+		{
+			_hashtable = new Hashtable();
+		}
+
+	    public IEnumerable<string> GetKeys() {
+	       return _hashtable.Keys.Cast<string>();
+	    }
+
+		public T Get(string key)
+		{
+			return (T)_hashtable[key];
+		}
+
+		public void Set(string key, T value)
+		{
+			_hashtable[key] = value;
+		}
+
+	    public bool Exists(string key) {
+	        return _hashtable.ContainsKey(key);
+	    }
+	}
+}
