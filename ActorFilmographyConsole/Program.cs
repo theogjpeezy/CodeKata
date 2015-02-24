a60ded34bdead39e6e322bcfaa4eaf22a51de788
justin.cronin01@gmail.com <justin.cronin01@gmail.com>
2015-02-23 23:58:19 -0500

    Changing example up to demonstrate Dependency injection.  I removed the metrics functionality as it wasn't necessary for this exercise.

---
+++ ActorFilmographyConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using filmography;
+
+namespace ActorFilmographyConsole {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Actor Filmography Reference!");
+            Console.Write("Please Enter an Actor's Name: ");
+            var input = Console.ReadLine();
+            var filmography = new Filmography();
+            PopulateStorage(filmography);
+
+            Console.Clear();
+            Console.WriteLine("{0} was featured in:", input);
+            foreach(var movie in filmography.GetActorFilms(input))
+            {
+                Console.WriteLine(movie);
+            }
+
+            Console.Read();
+        }
+
+        private static void PopulateStorage(Filmography filmography) {
+            filmography.AddActorFilm("Paul Walker", "The Fast and Furious");
+            filmography.AddActorFilm("Paul Walker", "Running Scared");
+            filmography.AddActorFilm("Al Pacino", "Scarface");
+            filmography.AddActorFilm("Al Pacino", "Heat");
+            filmography.AddActorFilm("Kevin Spacey", "Se7en");
+            filmography.AddActorFilm("Kevin Spacey", "K-Pax");
+        }
+    }
+}
---
+++ Filmography/Filmography.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DictionaryRepository;
+
+namespace filmography
+{
+    public class Filmography
+    {
+        public Filmography() {
+            _filmographyStorage = new DictionaryStorage<IEnumerable<string>>();
+        }
+
+        public IEnumerable<string> GetActors() {
+            return _filmographyStorage.GetKeys();
+        }
+
+        public IEnumerable<string> GetActorFilms(string actorName) {
+            return _filmographyStorage.Get(actorName);
+        }
+
+        public void AddActorFilm(string actorName, string movie) {
+            if (_filmographyStorage.Exists(actorName)) {
+                var movies = _filmographyStorage.Get(actorName).ToList();
+                movies.Add(movie);
+                _filmographyStorage.Set(actorName, movies);
+            }
+            else {
+                var movies = new List<string> { movie };
+                _filmographyStorage.Set(actorName, movies);
+            }
+        }
+
+        private readonly DictionaryStorage<IEnumerable<string>> _filmographyStorage;
+    }
+}
